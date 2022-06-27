3f6004d1365db891e9ba81373ed5b75d08012860
eror01 <eror.aleksandar01@gmail.com>
2022-06-27 22:49:59 +0200

    Add CRUD for TodoController

--- classes/TodoController.php
+++ classes/TodoController.php
@@ -37,10 +37,14 @@ class TodoController extends Db
       }
     }
   }
+  
+  // set Todo Content to display it in input
   public function setTodoContent($todoContent)
   {
     $this->todoContent = $todoContent;
   }
+
+  // get Todo Content from database and loop over it and setTodoContent
   public function getTodoContent($todo_edit_id, $session_id)
   {
     $stmt = $this->connect()->prepare('SELECT * FROM todos WHERE todo_id = ? AND todo_user_id = ?;');
@@ -51,15 +55,61 @@ class TodoController extends Db
       $this->setTodoContent($todo_content);
     }
   }
-  // function updateTodo($_GET[id], $_Session[id], todo-content_updated)
-  public function updateTodo($todo_id, $session_id, $todo_content_updated)
+ 
+  // update Todo
+  public function updateTodo($todo_id, $todo_content_updated)
   {
-
+    $stmt = $this->connect()->prepare('UPDATE todos SET todo_content = ? WHERE todo_id = ?;');
+    $stmt->execute(array($todo_content_updated, $todo_id));
   }
-  // function deleteTodo($_GET[id], $_Session[id])
-  // function updateTodoIsCompleted($_GET[id], todo_is_completed, $_Session[id])
-  // function showTodoIsComplete($_SESSION[id])
-  // function undoIsCompleted($_GET[id], $_SESSION[id]);
+
+  // delete Todo
+  public function deleteTodo($todo_id, $user_id)
+  {
+    $stmt = $this->connect()->prepare('DELETE FROM todos WHERE todo_id = ? AND todo_user_id = ?');
+    $stmt->execute(array($todo_id, $user_id));
+    header("Location: index.php");
+  }
+
+  // update Todo to completed
+  public function updateTodoIsCompleted($is_completed, $todo_is_completed = 0)
+  {
+    $stmt = $this->connect()->prepare('UPDATE todos SET todo_is_completed = ? WHERE todo_id = ?');
+    $stmt->execute(array($todo_is_completed = 1, $is_completed));
+    header("Location: index.php");
+  }
+
+  // undo Todo that is Completed
+  public function undoIsCompleted($user_id, $todo_is_completed = 0)
+  {
+    $stmt = $this->connect()->prepare('UPDATE todos SET todo_is_completed = ? WHERE todo_id = ?;');
+    $stmt->execute(array($todo_is_completed, $user_id));
+    header("Location: index.php");
+  }
+
+  // show Todos which are completed
+  public function showTodoIsComplete($session_id, $todo_is_completed = 1) 
+  {
+    $stmt = $this->connect()->prepare('SELECT * FROM todos WHERE todo_user_id = ? AND todo_is_completed = ? ');
+    $stmt->execute(array($session_id, $todo_is_completed));
+    if($stmt->rowCount() == 0) {
+      echo "<h2 class='text-center'>No completed todos</h2>";
+    } else {
+      $results = $todoCompleted = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      foreach($results as $row) {
+        $todo_id = $row['todo_id'];
+        $todo_content = $row['todo_content'];
+        $todo_content = ucfirst($todo_content);
+        echo "<ul class='todo-list_list'>";
+        echo "<li class='todo-list_item' style='background-color: #DCDCDC;'>";
+        echo "<p class='todo-list_title text-decoration-line-through'>{$todo_content}</p>";
+        echo "<a class='todo-list_link undo' href='index.php?is_completed_undo={$todo_id}' data-bs-toggle='tooltip' data-bs-placement='top' title='Undo'><i class='fa-solid fa-rotate-left'></i></a>";
+        echo "</li>";
+        echo "</ul>";
+      }
+    }
+  }
+
   
 
 
--- includes/todo_list.php
+++ includes/todo_list.php
@@ -1,17 +1,6 @@
 <?php
-include "classes/TodoController.php";
-if (isset($_SESSION['username'])) {
-  $username = $_SESSION['username'];
-  $userID = $_SESSION['userID'];
-  if (isset($_POST['create_todo'])) {
-    $todo_content = $_POST['todo-content'];
-    $todo_author = $username;
-    $todo_content = stripslashes($todo_content);
-    $todo = new TodoController();
-    $todo->createTodo($todo_content, $todo_author, $userID);
-  }
-}
-?>
+include "functions.php";
+createTodo(); ?>
 <section class="todo-list" id="todo">
   <div class="row  justify-content-center mb-4">
     <div class="col-12">
@@ -26,8 +15,14 @@ if (isset($_SESSION['username'])) {
     <div class="col-10">
       <?php if (isset($_GET['edit'])) :
         $todo_edit_id = $_GET['edit']; 
-        $todo = new TodoController(); 
-        $todo->setTodoContent($todo_edit_id, $userID)?>
+        $todoEdit = new TodoController(); 
+        $todoEdit->getTodoContent($todo_edit_id, $userID); 
+
+        if(isset($_POST['update_todo']))  {
+          $todo_content_updated = $_POST['todo-content_updated'];
+          $todoEdit->updateTodo($todo_edit_id, $todo_content_updated);
+        }
+        ?>
         <form action="" method="POST">
           <div class="input-group">
             <input type="text" class="form-control todo-input" value="<?php echo $todo->todoContent; ?>" name="todo-content_updated" id="todo-content" placeholder="Update your todo" required>
@@ -51,20 +46,20 @@ if (isset($_SESSION['username'])) {
   <div class="row justify-content-center">
     <div class="col-10">
       <?php
-
-      if (isset($userID)) {
-        $todoNotComplete = new TodoController();
-        $todoNotComplete->showTodoNotCompleted($userID);
-      }
-
-      ?>
+      updateTodoIsCompleted();
+      showTodoNotComplete(); ?>
     </div>
   </div>
-  <div class="row justify-content-center py-4 d-none">
+  <div class="row justify-content-center py-4">
     <div class="col-12">
       <h1 class="todo-list_user">Completed Todos</h1>
     </div>
     <div class="col-10">
+      <!-- show delete and updateiscompleted todo -->
+      <?php 
+      showTodosComplete();     
+      undoIsCompleted(); 
+      deleteTodo(); ?>
     </div>
   </div>
 </section>
